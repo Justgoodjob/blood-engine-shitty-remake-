b17109c7e97928d2e60bdf5ba91fea0479fca411
Algebra <81258093+Justgoodjob@users.noreply.github.com>
2022-05-01 18:50:29 -0500

    Add files via upload

---
+++ get all weapons.lua
@@ -0,0 +1,47 @@
+local args = {
+    [1] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("Players").LocalPlayer.PlayerGui.Main_Menu.Main.Events.Karambit_Event:FireServer(unpack(args))
+
+local args = {
+    [1] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("Players").LocalPlayer.PlayerGui.Main_Menu.Main.Events.SuperPunch_Event:FireServer(unpack(args))
+
+local args = {
+    [1] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("Players").LocalPlayer.PlayerGui.Main_Menu.Main.Events.SuperPunch_Event:FireServer(unpack(args))
+
+local args = {
+    [1] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("Players").LocalPlayer.PlayerGui.Main_Menu.Main.Events.SuperPunch_Event:FireServer(unpack(args))
+
+local args = {
+    [1] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("Players").LocalPlayer.PlayerGui.Main_Menu.Main.Events.SuperPunch_Event:FireServer(unpack(args))
+
+local args = {
+    [1] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("Players").LocalPlayer.PlayerGui.Main_Menu.Main.Events.SuperPunch_Event:FireServer(unpack(args))
+
+local args = {
+    [1] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("Players").LocalPlayer.PlayerGui.Main_Menu.Main.Events.SuperPunch_Event:FireServer(unpack(args))
+
+local args = {
+    [1] = game:GetService("Players").LocalPlayer
+}
+
+game:GetService("Players").LocalPlayer.PlayerGui.Main_Menu.Main.Events.SuperPunch_Event:FireServer(unpack(args))
