64e61c11a3bfa898745f28e704f02dae023fff13
Algebra <81258093+Justgoodjob@users.noreply.github.com>
2022-05-01 18:51:56 -0500

    Update get all weapons.lua

--- get all weapons.lua
+++ get all weapons.lua
@@ -8,25 +8,25 @@ local args = {
     [1] = game:GetService("Players").LocalPlayer
 }
 
-game:GetService("Players").LocalPlayer.PlayerGui.Main_Menu.Main.Events.SuperPunch_Event:FireServer(unpack(args))
+game:GetService("Players").LocalPlayer.PlayerGui.Main_Menu.Main.Events.Chainsaw_Event:FireServer(unpack(args))
 
 local args = {
     [1] = game:GetService("Players").LocalPlayer
 }
 
-game:GetService("Players").LocalPlayer.PlayerGui.Main_Menu.Main.Events.SuperPunch_Event:FireServer(unpack(args))
+game:GetService("Players").LocalPlayer.PlayerGui.Main_Menu.Main.Events.Katana_Event:FireServer(unpack(args))
 
 local args = {
     [1] = game:GetService("Players").LocalPlayer
 }
 
-game:GetService("Players").LocalPlayer.PlayerGui.Main_Menu.Main.Events.SuperPunch_Event:FireServer(unpack(args))
+game:GetService("Players").LocalPlayer.PlayerGui.Main_Menu.Main.Events.Fists_Event:FireServer(unpack(args))
 
 local args = {
     [1] = game:GetService("Players").LocalPlayer
 }
 
-game:GetService("Players").LocalPlayer.PlayerGui.Main_Menu.Main.Events.SuperPunch_Event:FireServer(unpack(args))
+game:GetService("Players").LocalPlayer.PlayerGui.Main_Menu.Main.Events.Bayonet_Event:FireServer(unpack(args))
 
 local args = {
     [1] = game:GetService("Players").LocalPlayer
